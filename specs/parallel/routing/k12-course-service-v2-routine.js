a533a81ca3870fc6d206ab85cc3a062b2e13afd7
Arnob <arnobxtreme@gmail.com>
2024-02-12 17:22:46 +0600

    update

--- specs/parallel/routing/k12-course-service-v2-routine.js
+++ specs/parallel/routing/k12-course-service-v2-routine.js
@@ -15,10 +15,10 @@ export default function () {
 
         let headers = {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDhkZGQzOTFjOTczMTE1YjcyN2I3ZCIsImlzX2FkbWluIjpmYWxzZSwiY29udGFjdCI6Iis4ODAxNTIxMjAwNDg4IiwiZW1haWwiOiJ0ZXN0QHRlc3Rlci5jb20iLCJsb2dpbl90eXBlIjoiZW1haWwiLCJsb2dpbl9zb3VyY2UiOiIxMG1pbnNjaG9vbCIsImxvZ2luX3RhcmdldCI6IiIsImxvZ2luX2FzIjowLCJuYW1lIjoiVGVzdCBBZ2FpbiIsImlzX2FjdGl2ZSI6ZmFsc2UsInZlcmlmaWVkIjp0cnVlLCJkZXZpY2VfaWQiOiI2NWMzNTE4YjY0NTEwNWU3OWRhYTkyYjciLCJkZXZpY2UiOnsiZGV2aWNlX2lkIjoiNjVjMzUxOGI2NDUxMDVlNzlkYWE5MmI3IiwiZGV2aWNlX25hbWUiOiJDaHJvbWUgMTIxLjAuMC4wIiwiZGV2aWNlX3R5cGUiOiJicm93c2VyIiwiZGV2aWNlX29zIjoiSW50ZWwgTWFjIE9TIFggMTBfMTVfNyIsIm9yaWdpbiI6Imh0dHBzOi8vYXBwLXN0YWdlLjEwbWludXRlc2Nob29sLm5ldCIsImlwX2FkZHJlc3MiOiIxNzUuNDEuNDUuMTk1In0sInVzZXJfc3RhdHVzIjoiIiwiZGF0ZV9qb2luZWQiOiIyMDIzLTAxLTMxVDA5OjIyOjI3LjMwN1oiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4MDE5MjExfQ.8iRJ9WEPiMD_GR7kmGxavb4mkn66WSmlz9zsCsUrgxk'
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDhkZGQzOTFjOTczMTE1YjcyN2I3ZCIsImlzX2FkbWluIjpmYWxzZSwiY29udGFjdCI6Iis4ODAxNTIxMjAwNDg4IiwiZW1haWwiOiJ0ZXN0QHRlc3Rlci5jb20iLCJsb2dpbl90eXBlIjoiZW1haWwiLCJsb2dpbl9zb3VyY2UiOiIxMG1pbnNjaG9vbCIsImxvZ2luX3RhcmdldCI6IiIsImxvZ2luX2FzIjowLCJuYW1lIjoiVGVzdCBBZ2FpbiIsImlzX2FjdGl2ZSI6ZmFsc2UsInZlcmlmaWVkIjp0cnVlLCJkZXZpY2VfaWQiOiI2NWM5ZmYyYzMzMWZkYzZmMWI0ZmVlMmYiLCJkZXZpY2UiOnsiZGV2aWNlX2lkIjoiNjVjOWZmMmMzMzFmZGM2ZjFiNGZlZTJmIiwiZGV2aWNlX25hbWUiOiJDaHJvbWUgMTEyLjAuMC4wIiwiZGV2aWNlX3R5cGUiOiJicm93c2VyIiwiZGV2aWNlX29zIjoiSW50ZWwgTWFjIE9TIFggMTBfMTVfNyIsIm9yaWdpbiI6Imh0dHBzOi8vYXBwLXN0YWdlLjEwbWludXRlc2Nob29sLm5ldCIsImlwX2FkZHJlc3MiOiIxNzUuNDEuNDUuMTk1In0sInVzZXJfc3RhdHVzIjoiIiwiZGF0ZV9qb2luZWQiOiIyMDIzLTAxLTMxVDA5OjIyOjI3LjMwN1oiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NDU2ODc2fQ.9wQ8O63TaYsgj1wZLYh03F6m6Wo5NyJGAx726OqK5J8'
         }
 
-        let res = http.get(`${baseUrl}/k12-course-service/api/v2/routine?start_at=2024-01-31%2000%3A00%3A00&end_at=2024-02-29%2000%3A00%3A00&catalog_product_id=10282`, {
+        let res = http.get(`${baseUrl}/k12-course-service/api/v2/routine?start_at=2024-01-31%2000%3A00%3A00&end_at=2024-02-29%2000%3A00%3A00`, {
             headers: headers
         });
 
