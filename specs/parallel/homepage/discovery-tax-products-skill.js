adae3392dbcc030c9f16b0c26e35e4b583c36d7e
Arnob Almazee <arnobxtreme@gmail.com>
2024-02-12 13:09:32 +0000

    update

--- specs/parallel/homepage/discovery-tax-products-skill.js
+++ specs/parallel/homepage/discovery-tax-products-skill.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 //Define the stages for the test
 export let options = {
-    vus: 50000,
+    vus: 25000,
     duration: '30s',
 };
 
@@ -13,32 +13,31 @@ const baseUrl = 'https://stage-api.10minuteschool.net';
 
 export default function () {
 
-        let headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDhkZGQzOTFjOTczMTE1YjcyN2I3ZCIsImlzX2FkbWluIjpmYWxzZSwiY29udGFjdCI6Iis4ODAxNTIxMjAwNDg4IiwiZW1haWwiOiJ0ZXN0QHRlc3Rlci5jb20iLCJsb2dpbl90eXBlIjoiZW1haWwiLCJsb2dpbl9zb3VyY2UiOiIxMG1pbnNjaG9vbCIsImxvZ2luX3RhcmdldCI6IiIsImxvZ2luX2FzIjowLCJuYW1lIjoiVGVzdCBBZ2FpbiIsImlzX2FjdGl2ZSI6ZmFsc2UsInZlcmlmaWVkIjp0cnVlLCJkZXZpY2VfaWQiOiI2NWM5ZmYyYzMzMWZkYzZmMWI0ZmVlMmYiLCJkZXZpY2UiOnsiZGV2aWNlX2lkIjoiNjVjOWZmMmMzMzFmZGM2ZjFiNGZlZTJmIiwiZGV2aWNlX25hbWUiOiJDaHJvbWUgMTEyLjAuMC4wIiwiZGV2aWNlX3R5cGUiOiJicm93c2VyIiwiZGV2aWNlX29zIjoiSW50ZWwgTWFjIE9TIFggMTBfMTVfNyIsIm9yaWdpbiI6Imh0dHBzOi8vYXBwLXN0YWdlLjEwbWludXRlc2Nob29sLm5ldCIsImlwX2FkZHJlc3MiOiIxNzUuNDEuNDUuMTk1In0sInVzZXJfc3RhdHVzIjoiIiwiZGF0ZV9qb2luZWQiOiIyMDIzLTAxLTMxVDA5OjIyOjI3LjMwN1oiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NDU2ODc2fQ.9wQ8O63TaYsgj1wZLYh03F6m6Wo5NyJGAx726OqK5J8'
-        }
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDhkZGQzOTFjOTczMTE1YjcyN2I3ZCIsImlzX2FkbWluIjpmYWxzZSwiY29udGFjdCI6Iis4ODAxNTIxMjAwNDg4IiwiZW1haWwiOiJ0ZXN0QHRlc3Rlci5jb20iLCJsb2dpbl90eXBlIjoiZW1haWwiLCJsb2dpbl9zb3VyY2UiOiIxMG1pbnNjaG9vbCIsImxvZ2luX3RhcmdldCI6IiIsImxvZ2luX2FzIjowLCJuYW1lIjoiVGVzdCBBZ2FpbiIsImlzX2FjdGl2ZSI6ZmFsc2UsInZlcmlmaWVkIjp0cnVlLCJkZXZpY2VfaWQiOiI2NWM5ZmYyYzMzMWZkYzZmMWI0ZmVlMmYiLCJkZXZpY2UiOnsiZGV2aWNlX2lkIjoiNjVjOWZmMmMzMzFmZGM2ZjFiNGZlZTJmIiwiZGV2aWNlX25hbWUiOiJDaHJvbWUgMTEyLjAuMC4wIiwiZGV2aWNlX3R5cGUiOiJicm93c2VyIiwiZGV2aWNlX29zIjoiSW50ZWwgTWFjIE9TIFggMTBfMTVfNyIsIm9yaWdpbiI6Imh0dHBzOi8vYXBwLXN0YWdlLjEwbWludXRlc2Nob29sLm5ldCIsImlwX2FkZHJlc3MiOiIxNzUuNDEuNDUuMTk1In0sInVzZXJfc3RhdHVzIjoiIiwiZGF0ZV9qb2luZWQiOiIyMDIzLTAxLTMxVDA5OjIyOjI3LjMwN1oiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4NDU2ODc2fQ.9wQ8O63TaYsgj1wZLYh03F6m6Wo5NyJGAx726OqK5J8'
+    }
 
-        let res = http.get(`${baseUrl}/discovery-service/api/v1/taxonomies/products?vertical=skills&segment=language-learning&product_limit=10`, {
-            headers: headers
-        });
+    let res = http.get(`${baseUrl}/discovery-service/api/v1/taxonomies/products?vertical=skills&segment=language-learning&product_limit=10`, {
+        headers: headers
+    });
 
-        check(res, {
-            'status is 200': (r) => r.status === 200,
-            'status is 502': (r) => r.status === 502,
-            'status is 503': (r) => r.status === 503,
-            'status is 500': (r) => r.status === 500,
-            'status is 520': (r) => r.status === 520,
-            'status is 521': (r) => r.status === 521,
-            'status is 522': (r) => r.status === 522,
-            'response time < 1000ms': (r) => r.timings.duration < 1000
-        });
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'status is 502': (r) => r.status === 502,
+        'status is 503': (r) => r.status === 503,
+        'status is 500': (r) => r.status === 500,
+        'status is 520': (r) => r.status === 520,
+        'status is 521': (r) => r.status === 521,
+        'status is 522': (r) => r.status === 522,
+        'response time < 1000ms': (r) => r.timings.duration < 1000
+    });
 
-        // if (res.status !== 200) {
-        //     console.log("discovery-service/api/v1/taxonomies/products?vertical=skills > Status: " + res.status);
-        // }
-       
+    // if (res.status !== 200) {
+    //     console.log("discovery-service/api/v1/taxonomies/products?vertical=skills > Status: " + res.status);
+    // }
 
-        sleep(2);
+    sleep(1);
 }
 
 
